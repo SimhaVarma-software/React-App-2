7187a8e5e81f917348a06602d2303a8ac98157bb
SimhaVarma-software <simhavarma79@gmail.com>
2025-03-28 13:34:33 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Simha varma</h1>
+      <h2>simhavarma79@gmail.com</h2>
+    </>
+  )
+}
+
+export default App
